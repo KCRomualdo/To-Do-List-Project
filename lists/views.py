5c2eaeb7bb6523ef81db56e9f59c6b6ede22479f
krista <krista@example.com>
2015-11-11 15:16:28 +0800

    Commit: FT refractor

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 #from django.http import HttpResponse
 
@@ -8,4 +9,9 @@ from django.shortcuts import render
 #	return HttpResponse('<html><title>To-Do lists</title></html>')
 
 def home_page(request):
-	return render(request,'home.html')
+	#if request.method=='POST':
+	#	return HttpResponse(request.POST['item_text'])
+	#return render(request,'home.html')
+	return render(request,'home.html',{
+		'new_item_text':request.POST.get('item_text',''),
+	})
