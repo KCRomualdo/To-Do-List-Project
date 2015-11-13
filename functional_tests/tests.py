29e956552d0caea4b5167f96581835c8ac3d5b4a
krista <kcromualdo14@yahoo.com>
2015-11-13 12:40:10 +0800

    new URL + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -53,17 +53,12 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox=self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-		
-		# The page updates again, and now shows both items on her list
-
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
-		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		#The page updates again, and now shows both items on her list
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-		#NOw a new user, FRancis comes aling to the site
+		#Now a new user, FRancis comes aling to the site
 
 		##We use a new browser session to make sure that no information
 		##of Edith's is coming through from cookies etc
@@ -89,7 +84,7 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertNotEqual(francis_lists_url,edith_list_url)
 
 		#AGain, there is no trace of Edith's list
-		page_text=self.browser/find_element_by_tag_name('body').text
+		page_text=self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers',page_text)
 		self.assertIn('Buy milk',page_text)
 
