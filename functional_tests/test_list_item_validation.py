669e3b32f510d90d009b8339321f6e94b6231438
krista <kcromualdo14@yahoo.com>
2015-11-17 16:14:22 +0800

    Rename all item input ids and names. still broken

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -10,7 +10,8 @@ class ItemValidationTest(FunctionalTest):
 		#Edith goes to the home 	page and accidentally tries to submit
 		#an empty list item. She hits Enter on the empty input box
 		self.browser.get(self.server_url)
-		self.browser.find_element_by_id('id_new_item').send_keys('\n')
+		#self.browser.find_element_by_id('id_new_item').send_keys('\n')
+		self.get_item_input_box().send_keys('\n')
 
 		#The home page refreshes, and there is an error message saying
 		#that list items cannot be blank
@@ -22,7 +23,8 @@ class ItemValidationTest(FunctionalTest):
 		self.check_for_row_in_list_table('1:Buy milk')
 
 		#Perversely, she now decides to submit a second blank list item
-		self.browser.find_element_by_id('id_new_item').send_keys('\n')
+		#self.browser.find_element_by_id('id_new_item').send_keys('\n')
+		self.get_item_input_box().send_keys('\n')
 
 		#She receieves a similar warning on the list page
 		self.check_for_row_in_list_table('1:Buy milk')
--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -20,7 +20,8 @@ class NewVisitorTest(FunctionalTest):
 		self.assertIn('To-Do',header_text)
 
 		# She is invited to enter a to-do item straight away
-		inputbox=self.browser.find_element_by_id('id_new_item')
+		#inputbox=self.browser.find_element_by_id('id_new_item')
+		inputbox=self.get_item_input_box()
 		self.assertEqual(
 				inputbox.get_attribute('placeholder'),
 				'Enter a to-do item'
@@ -40,7 +41,8 @@ class NewVisitorTest(FunctionalTest):
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
 		#self.fail('Finish the test!')
 
-		inputbox=self.browser.find_element_by_id('id_new_item')
+		#inputbox=self.browser.find_element_by_id('id_new_item')
+		inputbox=self.get_item_input_box()
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 
@@ -64,7 +66,8 @@ class NewVisitorTest(FunctionalTest):
 
 		#Francis starts a new list bu entering a nre item. He
 		#is less intersting than Edith
-		inputbox=self.browser.find_element_by_id('id_new_item')
+		#inputbox=self.browser.find_element_by_id('id_new_item')
+		inputbox=self.get_item_input_box()
 		inputbox.send_keys('Buy milk')
 		inputbox.send_keys(Keys.ENTER)
 
